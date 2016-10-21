f9eeba9036a35418ec5e85524d0a4b1cf69de03d
KTB10 <ktb10@pct.edu>
2016-10-21 14:41:04 -0400

    Updated existing file

--- TwelveDays.java
+++ TwelveDays.java
@@ -16,33 +16,33 @@ public class TwelveDays
 
 	  // convert to switch
       switch(choice) {
-      case 12: thing = "Twelve drummers drumming";
-      case 11: thing = "Eleven pipers piping";
-      case 10: thing = "Ten lords a-leaping";
-      case 9:  thing = "Nine ladies dancing";
-      case 8:  thing = "Eight maids a-milking";
-      case 7:  thing = "Seven swans a-swimming";
-      case 6:  thing = "Six geese a-laying";
-      case 5:  thing = "Five golden rings";
-      case 4:  thing = "Four calling birds";
-      case 3:  thing = "Three French hens";
-      case 2:  thing = "Two turtle doves and";
-      case 1:  thing = "A partridge in a pear tree";
-      default: thing = "Invalid choice";
+      case 12: thing = "Twelve drummers drumming"; break;
+      case 11: thing = "Eleven pipers piping"; break;
+      case 10: thing = "Ten lords a-leaping"; break;
+      case 9:  thing = "Nine ladies dancing"; break;
+      case 8:  thing = "Eight maids a-milking"; break;
+      case 7:  thing = "Seven swans a-swimming"; break;
+      case 6:  thing = "Six geese a-laying"; break;
+      case 5:  thing = "Five golden rings"; break;
+      case 4:  thing = "Four calling birds"; break;
+      case 3:  thing = "Three French hens"; break;
+      case 2:  thing = "Two turtle doves and"; break;
+      case 1:  thing = "A partridge in a pear tree"; break;
+      default: thing = "Invalid choice"; break;
       }
 	   
 	  // This is ugly... let's also convert it to a switch... this is just a little trickier
       if(choice >= 4) choice = 4;
       switch(choice) {
-      case 4: suffix = "th";
-      case 3: suffix = "rd";
-      case 2: suffix = "nd";
-      default: suffix = "st";
+      case 4: suffix = "th"; break;
+      case 3: suffix = "rd"; break;
+      case 2: suffix = "nd"; break;
+      default: suffix = "st"; break;
       }
       
 	  System.out.println("\nOn the " + choice + suffix +
           " day of Christmas\n" +
-          "My true love gave to me" + thing);
+          "My true love gave to me\n" + thing);
    }
    
    // custom method to get user input with GUI. Returns their day choice.
